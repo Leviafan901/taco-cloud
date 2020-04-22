5c5fe1c955447fadb818e10daddaa91ba066f3bb
Aliaksei Sosenkov <Aliaksei_Sosenkov@epam.com>
2020-04-23 00:38:37 +0300

    Design page is ready

---
+++ src/main/java/sia/tacocloud/tacos/Taco.kt
@@ -0,0 +1,10 @@
+package sia.tacocloud.tacos
+
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+data class Taco(@NotNull
+                @Size(min=5, message="Name must be at least 5 characters long")
+                var name: String = "Taco's Name",
+                @Size(min=1, message="You must choose at least 1 ingredient")
+                var ingredients: List<Ingredient> = ArrayList<Ingredient>())
